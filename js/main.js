ac2baa55b91363da95d1bb1845cd73bacdc5e775
Sean Xavier <seanxhallion@gmail.com>
2017-03-02 19:15:17 -0500

    creating variables for slide length & count

---
+++ js/main.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+
+	// show the first img in our slideshow by default
+	$('img').eq(0).show();
+	// create a var to keep track of the index of the img we want to work with
+	var currentSlideIndex = 0;
+	// the number of images in our slideshow
+	var slideCount = $('img').length;
+
+
+}); // closes doc.ready
